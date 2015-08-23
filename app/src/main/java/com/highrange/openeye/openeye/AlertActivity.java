0c58495139d770545c859ba847636cb5d8d3b21b
Mahesh C <mahesh.fisat@gmail.com>
2015-08-23 09:33:29 +0530

    Initial commit without doc

--- app/src/main/java/com/highrange/openeye/openeye/AlertActivity.java
+++ app/src/main/java/com/highrange/openeye/openeye/AlertActivity.java
@@ -15,23 +15,12 @@ import android.os.CountDownTimer;
 import android.widget.TextView;
 import android.telephony.SmsManager;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
+
 public class AlertActivity extends Activity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
+
     private static final boolean AUTO_HIDE = true;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
+
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
     /**
@@ -40,14 +29,10 @@ public class AlertActivity extends Activity {
      */
     private static final boolean TOGGLE_ON_CLICK = true;
 
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
+
     private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
 
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
+
 
 
     SimulationData data=new SimulationData();
@@ -81,9 +66,7 @@ public class AlertActivity extends Activity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
+
 
     }
 
